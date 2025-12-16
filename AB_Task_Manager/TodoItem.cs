83760321a466af8d24f809923ca1d2c31aa403ca
Adrian Babiński <adrian.babinski@protonmail.com>
2025-12-16 19:10:29 +0100

    Add project files.

---
+++ AB_Task_Manager/TodoItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AB_Task_Manager
+{
+    // MODEL
+    // To jest zwykła klasa (POCO - Plain Old CLR Object).
+    // Reprezentuje ona nasze dane, np. rekord z bazy danych.
+    internal class TodoItem
+    {
+        public string Title { get; set; } = string.Empty;
+        public bool IsDone { get; set; }
+    }
+}
